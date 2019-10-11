2a14453c76c580a0f043389399a630fecc93b92a
Prabhat Tyagi <harrytyagi2371@gmail.com>
2019-10-11 14:36:37 +0530

    Added 2 more team members

--- src/Register.js
+++ src/Register.js
@@ -10,10 +10,16 @@ class Register extends React.Component {
       teamName:'',
       candidateName1:'',
       candidateName2:'',
+	  candidateName3:'',
+      candidateName4:'',
       candidatePhoneNumber1:'',
       candidatePhoneNumber2:'',
+	  candidatePhoneNumber3:'',
+      candidatePhoneNumber4:'',
       candidateEmailID1:'',
       candidateEmailID2:'',
+	  candidateEmailID3:'',
+      candidateEmailID4:'',
       toShowLoader:false,
       toShowBlurBackGround: false,
       teamAvailableMessage: '',
@@ -23,10 +29,16 @@ class Register extends React.Component {
     this.handleTeamChange = this.handleTeamChange.bind(this);
     this.handleNameChange1 = this.handleNameChange1.bind(this);
     this.handleNameChange2 = this.handleNameChange2.bind(this);
+	  this.handleNameChange3 = this.handleNameChange3.bind(this);
+    this.handleNameChange4 = this.handleNameChange4.bind(this);
     this.handlePhoneNumberChange1 = this.handlePhoneNumberChange1.bind(this);
     this.handlePhoneNumberChange2 = this.handlePhoneNumberChange2.bind(this);
+	  this.handlePhoneNumberChange3 = this.handlePhoneNumberChange3.bind(this);
+    this.handlePhoneNumberChange4 = this.handlePhoneNumberChange4.bind(this);
     this.handleEmailIDChange1 = this.handleEmailIDChange1.bind(this);
     this.handleEmailIDChange2 = this.handleEmailIDChange2.bind(this);
+	  this.handleEmailIDChange3 = this.handleEmailIDChange3.bind(this);
+    this.handleEmailIDChange4 = this.handleEmailIDChange4.bind(this);
     this.registerTeam = this.registerTeam.bind(this);
     
 }
@@ -43,6 +55,14 @@ handleNameChange2(event){
   this.setState({candidateName2:event.target.value});
 }
 
+handleNameChange3(event){
+    this.setState({candidateName3:event.target.value});
+}
+
+handleNameChange4(event){
+  this.setState({candidateName4:event.target.value});
+}
+
 handlePhoneNumberChange1(event){
   this.setState({candidatePhoneNumber1:event.target.value});
 }
@@ -51,12 +71,28 @@ handlePhoneNumberChange2(event){
   this.setState({candidatePhoneNumber2:event.target.value});
 }
 
+handlePhoneNumberChange3(event){
+  this.setState({candidatePhoneNumber3:event.target.value});
+}
+
+handlePhoneNumberChange4(event){
+  this.setState({candidatePhoneNumber4:event.target.value});
+}
+
 handleEmailIDChange1(event){
   this.setState({candidateEmailID1:event.target.value});
 }
 
 handleEmailIDChange2(event){
   this.setState({candidateEmailID2:event.target.value});
+}
+
+handleEmailIDChange3(event){
+  this.setState({candidateEmailID3:event.target.value});
+}
+
+handleEmailIDChange4(event){
+  this.setState({candidateEmailID4:event.target.value});
 }
 
 registerTeam(){
@@ -69,7 +105,10 @@ registerTeam(){
       method: "POST",
       body: JSON.stringify(
       [{"EmailID":this.state.candidateEmailID1,"PhoneNumber":this.state.candidatePhoneNumber1,"Name":this.state.candidateName1,"TeamName":this.state.teamName,"status":null},
-      {"EmailID":this.state.candidateEmailID2,"PhoneNumber":this.state.candidatePhoneNumber2,"Name":this.state.candidateName2,"TeamName":this.state.teamName,"status":null}])
+      {"EmailID":this.state.candidateEmailID2,"PhoneNumber":this.state.candidatePhoneNumber2,"Name":this.state.candidateName2,"TeamName":this.state.teamName,"status":null},
+	  {"EmailID":this.state.candidateEmailID3,"PhoneNumber":this.state.candidatePhoneNumber3,"Name":this.state.candidateName3,"TeamName":this.state.teamName,"status":null},
+	  {"EmailID":this.state.candidateEmailID4,"PhoneNumber":this.state.candidatePhoneNumber4,"Name":this.state.candidateName4,"TeamName":this.state.teamName,"status":null}
+	  ])
       })
       .then(res => res.json())
       .then(
@@ -162,6 +201,36 @@ render() {
                 <div className="emailIDDiv">
                     <div className="emailIDInputTitle">Email ID</div>
                     <input className="emailIDInput" type="text" placeholder="Enter Email ID" onChange={this.handleEmailIDChange2}/>
+                </div>
+            </div>
+			<div className="candidateForm1">
+                <div className="candidateFormHeader">Candidate 3</div>
+                <div className="nameDiv">
+                    <div className="nameInputTitle">Name</div>
+                    <input className="nameInput" type="text" placeholder="Enter name" onChange={this.handleNameChange3}/>
+                </div>
+                <div className="phoneNumberDiv">
+                    <div className="phoneNumberInputTitle">Phone Number</div>
+                    <input className="phoneNumberInput" type="text" placeholder="Enter phone number" onChange={this.handlePhoneNumberChange3}/>
+                </div>
+                <div className="emailIDDiv">
+                    <div className="emailIDInputTitle">Email ID</div>
+                    <input className="emailIDInput" type="text" placeholder="Enter Email ID" onChange={this.handleEmailIDChange3}/>
+                </div>
+            </div>
+			<div className="candidateForm1">
+                <div className="candidateFormHeader">Candidate 4</div>
+                <div className="nameDiv">
+                    <div className="nameInputTitle">Name</div>
+                    <input className="nameInput" type="text" placeholder="Enter name" onChange={this.handleNameChange4}/>
+                </div>
+                <div className="phoneNumberDiv">
+                    <div className="phoneNumberInputTitle">Phone Number</div>
+                    <input className="phoneNumberInput" type="text" placeholder="Enter phone number" onChange={this.handlePhoneNumberChange4}/>
+                </div>
+                <div className="emailIDDiv">
+                    <div className="emailIDInputTitle">Email ID</div>
+                    <input className="emailIDInput" type="text" placeholder="Enter Email ID" onChange={this.handleEmailIDChange4}/>
                 </div>
             </div>
             <div className="registerButton" onClick={ this.registerTeam }>Submit</div>
